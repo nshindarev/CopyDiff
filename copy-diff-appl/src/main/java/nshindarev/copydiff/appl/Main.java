1f4d3ed94d2c3c9b2108788613e4cb1977d04241
Shindarev Nikita <nshindarev@gmail.com>
2016-08-17 11:43:12 +0300

    Добавлено логирование через связку slf4g/logback

--- copy-diff-appl/src/main/java/nshindarev/copydiff/appl/Main.java
+++ copy-diff-appl/src/main/java/nshindarev/copydiff/appl/Main.java
@@ -1,12 +1,17 @@
 package nshindarev.copydiff.appl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by nshindarev on 16.08.16.
  */
 public class Main {
 
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("Application started.");
+        logger.info("Application started.");
     }
 
 }
