64f9af5b3656125435159846f4c73b57b64643a9
Shindarev Nikita <nshindarev@gmail.com>
2016-08-19 23:16:02 +0300

    Поправлен файл copydiff.sh. При модификации после copy/past потерялся символ $ :(

--- copy-diff-appl/src/assembly/bin/copydiff.sh
+++ copy-diff-appl/src/assembly/bin/copydiff.sh
@@ -7,4 +7,4 @@ copydiff_home=$( cd "$(dirname "${BASH_SOURCE}")" ; cd .. ; pwd -P )
 if [ ! -d "$copydiff_home/log" ]; then
     mkdir $copydiff_home/log >/dev/null 2>/dev/null
 fi
-java -cp "$copydiff_home/conf/:$copydiff_home/lib/*" $main_class *
+java -cp "$copydiff_home/conf/:$copydiff_home/lib/*" $main_class $*
